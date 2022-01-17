483d71a7bf243c5dad576ca85b82a207f250093e
角山翔太朗 <78521821+kakuyama-shotaro@users.noreply.github.com>
2022-01-18 00:01:41 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PrimaryButton>test</PrimaryButton>
+      <SecondaryButton>確認</SecondaryButton>
+    </div>
+  );
+}
---
+++ src/components/atoms/button/BaseButton.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+  border: none;
+  border-radius: 9999px;
+  color: #ffffff;
+  outline: none;
+  padding: 6px 24px;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+`;
